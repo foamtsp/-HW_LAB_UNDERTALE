2847bf611603cd78fa437ce478a1f86f3815d39d
foamtsp <foamrock@gmail.com>
2020-05-22 22:21:45 +0700

    add sample file

---
+++ undertale_project.srcs/sim_1/new/test.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 05/22/2020 09:59:51 PM
+// Design Name: 
+// Module Name: test
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module test(
+
+    );
+endmodule
